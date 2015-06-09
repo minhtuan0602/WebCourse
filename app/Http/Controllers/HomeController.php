b9dcb309b56b98a0724772dcdc5b0d14ce9a5f67
SherLock Tuan <minhtuan0602@gmail.com>
2015-06-09 15:29:57 +0700

    change position to integer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,12 @@ class HomeController extends Controller {
 
 	public function index()
 	{
-    $category_main = Category::where('isBuiltIn', '=', 1)->get();
-    $category_news = Category::where('isBuiltIn', '=', 0)->get();
-    $articles_training = Article::where('category_id', '=', 1)->get();
-    $articles_research_type_1 = Article::where('category_id', '=', 3)->where('type', '=', 1)->get();
-    $articles_research_type_2 = Article::where('category_id', '=', 3)->where('type', '=', 2)->get();
-    $articles_history = Article::where('category_id', '=', 4)->get();
+    $category_main = Category::where('isBuiltIn', '=', 1)->orderBy('position', 'asc')->get();
+    $category_news = Category::where('isBuiltIn', '=', 0)->orderBy('position', 'asc')->get();
+    $articles_training = Article::where('category_id', '=', 1)->orderBy('position', 'asc')->get();
+    $articles_research_type_1 = Article::where('category_id', '=', 3)->where('type', '=', 1)->orderBy('position', 'asc')->get();
+    $articles_research_type_2 = Article::where('category_id', '=', 3)->where('type', '=', 2)->orderBy('position', 'asc')->get();
+    $articles_history = Article::where('category_id', '=', 4)->orderBy('position', 'asc')->get();
 		return view('home', compact('category_main', 'category_news', 'articles_training', 'articles_research_type_1', 'articles_research_type_2', 'articles_history'));
 	}
 
